653638f42cd8b5db2af9fbc09075de7fe1648ca8
Frazier Odhiambo <ofrazy@gmail.com>
2022-07-25 16:03:28 +0300

    object constructor problem solved, faster load time

--- public/main.js
+++ public/main.js
@@ -10,26 +10,26 @@ async function hosiApiRequest(){
     try{
         const response = await fetch(hosiUrl)
         const data = await response.json()
-        console.log(data)
+        console.log(data.length)
 
        
-        let institution = []
-        for (let i=0;i<data.result.total;i++){
+        // let institution = []
+        // for (let i=0;i<data.result.total;i++){
         
-        function DisplayFacility(data) {
-            this.facility = data.result.hits[i]._source.name;
-            this.type_facility = data.result.hits[i]._source.facility_type_category
-            this.owner = data.result.hits[i]._source.owner_type_name
-            this.county_name = data.result.hits[i]._source.county_name
-            this.services = data.result.hits[i]._source.service_names
+        // function DisplayFacility(data) {
+        //     this.facility = data.result.hits[i]._source.name;
+        //     this.type_facility = data.result.hits[i]._source.facility_type_category
+        //     this.owner = data.result.hits[i]._source.owner_type_name
+        //     this.county_name = data.result.hits[i]._source.county_name
+        //     this.services = data.result.hits[i]._source.service_names
 
-        }
+        // }
          
-        let hosi = new DisplayFacility(data)
+        // let hosi = new DisplayFacility(data)
 
-        institution.push(hosi)
-            } 
-            console.log(institution)
+        // institution.push(hosi)
+        //     } 
+        //     console.log(institution)
     }catch(error){
         console.log(error)
     }
--- server.js
+++ server.js
@@ -39,6 +39,8 @@ app.get('/hospital/:facilityName', async (req,res)=>{
     const facilityFetch = await fetch(
         `https://api.healthtools.codeforafrica.org/search/health-facilities?q=[${institution}]&per_page=1000`
     )
+
+    //fetch the data then parse it to json 
     .then((res) => {
         if (res.ok){
             return res.json()
@@ -47,9 +49,32 @@ app.get('/hospital/:facilityName', async (req,res)=>{
         }
     })
     
+    //here we retrive the data then select what we need and create the institution object
     .then (data => {
-        console.log(data)
-        res.send(data)
+        let institution = []
+        //the institution array will hold all data returned from the search
+        for (let i=0;i<data.result.total;i++){
+        
+            //this is the object contructor for each single hospital facility
+            //it will contain all the key information like name, county,location and serices offered
+        function DisplayFacility(data) {
+            this.facility = data.result.hits[i]._source.name;
+            this.type_facility = data.result.hits[i]._source.facility_type_category
+            this.owner = data.result.hits[i]._source.owner_type_name
+            this.county_name = data.result.hits[i]._source.county_name
+            this.services = data.result.hits[i]._source.service_names
+
+        }
+         
+        let hosi = new DisplayFacility(data)
+
+        //each new object will be called hosi(common short name for hospital)
+        institution.push(hosi)
+        //each object will then be pushed to the institution array
+    }
+        JSON.stringify(institution)
+        //the array will then be converted to json through the stringify() function and then sent out as json for the main fetch request
+        res.send(institution)
         
     })
     
